20bdfe6f6c5d662786aa15f89287655c6dfe5418
sicelo17 <70627871+sicelo17@users.noreply.github.com>
2021-07-04 11:35:09 +0200

    added product model

--- familyTreeLite/product/models.py
+++ familyTreeLite/product/models.py
@@ -18,3 +18,23 @@ class Category(models.Model):
 
     def get_absolute_url(self):
         return f'/{self.slug}/'
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    slug = models.SlugField()
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(upload_to='uploads/', blank=True, null=True)
+    thumbnail = models.ImageField(upload_to='uploads/', blank=True, null=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-date_added',)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f'/{self.category.slug}/{self.slug}/'
