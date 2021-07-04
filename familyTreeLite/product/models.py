a03b3154d655ec90cc7986af3b12d0f751ae734f
sicelo17 <70627871+sicelo17@users.noreply.github.com>
2021-07-04 11:01:29 +0200

    added category models

--- familyTreeLite/product/models.py
+++ familyTreeLite/product/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Models):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField()  #address version of the name
+    
+    #ordering  categories by name in the backend
+    class Meta:
+        ordering = ('name')
+        
+    
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f'/{self.slug}/'
