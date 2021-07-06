bd20477b859775468924c3ba0750c98faccd7749
sicelo17 <70627871+sicelo17@users.noreply.github.com>
2021-07-06 19:35:56 +0200

    added product detail

--- familyTreeLite/product/views.py
+++ familyTreeLite/product/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.http import Http404
 
 from rest_framework.views import APIView
 from rest_framework.response import Response 
@@ -11,3 +11,16 @@ class LatestProductsList(APIView):
         products = Product.objects.all()[0:4]  #getting the first four items from the database
         serializer = ProductSerializer(products, many=True)
         return Response(serializer.data)
+
+
+class ProductDetail(APIView):
+    def get_object(self, category_slug, product_slug):
+        try:
+            return Product.objects.filter(category__slug=category_slug).get(slug=product_slug)
+        except Product.DoesNotExist:
+            raise Http404
+
+    def get(self, category_slug, product_slug, format=None):
+        product = self.get_object(category_slug, product_slug)
+        serializer = ProductSerializer(product)     #getting data from serializers.py
+        return Response(seriaizer.data)
--- familytreelite-vue/src/views/Home.vue
+++ familytreelite-vue/src/views/Home.vue
@@ -2,29 +2,29 @@
   <div class="home">
     <section class="hero is-medium is-dark mb-6">
       <div class="hero-body has-text-centered">
-        <p class="title mb-6">
-          Welcome to Family Tree Lite
-        </p>
+        <p class="title mb-6">Welcome to Family Tree Lite</p>
 
-        <p class="subtitle">
-          Bringing the whole shop to you
-        </p>
+        <p class="subtitle">Bringing the whole shop to you</p>
       </div>
     </section>
 
-    <div class="columns is-multi-line">
+    <div class="columns is-multiline">
       <div class="column is-12">
-        <h2 class="is-size-2 has-text-centered">Lastest Products</h2>
+        <h2 class="is-size-2 has-text-centered">Latest Products</h2>
       </div>
 
-      <div class="column is-3"  v-for="product in latestProducts" :key="product.id">
+      <div
+        class="column is-3"
+        v-for="product in latestProducts"
+        :key="product.id"
+      >
         <div class="box">
           <figure class="image mb-4">
             <img :src="product.get_thumbnail">
           </figure>
 
           <h3 class="is-size-4">{{ product.name }}</h3>
-          <p class="is-size-6 has-text-grey">{{ product.price }}</p>
+          <p class="is-size-6 has-text-grey">${{ product.price }}</p>
 
           View Details
         </div>
@@ -37,35 +37,33 @@
 import axios from 'axios'
 
 export default {
-  name: 'Home',
-  data(){
-    return{
-      latestProducts: []
-    }
+  name: "Home",
+  data() {
+    return {
+      latestProducts: [],
+    };
   },
-  components: {
-  },
-  mounted(){
-    this.getLatestProducts
+  components: {},
+  mounted() {
+    this.getLatestProducts();
   },
   methods: {
     getLatestProducts() {
       axios
-        .get('/api/v1/latest-products/')
-        .then (response => {
-          this.latestProducts = response.data
+        .get("/api/v1/latest-products/")
+        .then((response) => {
+          this.latestProducts = response.data;
         })
-        .catch(error => {
-          console.log(error)
-        })
-    }
-  }
-
-}
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+};
 </script>
 
 <style scoped>
-  .image{
-    margin: -1.25rem -1.25rem 0 -1.25rem;
-  }
+.image {
+  margin: -1.25rem -1.25rem 0 -1.25rem;
+}
 </style>
