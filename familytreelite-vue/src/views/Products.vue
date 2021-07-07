ad1ff396aacf9ec7ba48f0c8af222f4a35867547
sicelo17 <70627871+sicelo17@users.noreply.github.com>
2021-07-07 15:36:58 +0200

    added product methods

--- familytreelite-vue/src/views/Products.vue
+++ familytreelite-vue/src/views/Products.vue
@@ -37,7 +37,17 @@ export default {
   name: "Product",
   data() {
       return {
+          product: {},
           quantity: 1,
+      }
+  },
+  mounted() {
+      this.getProduct()
+  },
+  methods: {
+      getProduct(){
+        const category_slug = this.$route.params.category_slug
+        const product_slug = this.$route.params.product_slug
       }
   }
 };
