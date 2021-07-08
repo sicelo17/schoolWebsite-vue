391d80778ba713fef42d5f1e9f82d56ba5a43c42
sicelo17 <70627871+sicelo17@users.noreply.github.com>
2021-07-08 13:17:48 +0200

    added loading bar to home.vue

--- familytreelite-vue/src/views/Home.vue
+++ familytreelite-vue/src/views/Home.vue
@@ -48,16 +48,18 @@ export default {
     this.getLatestProducts();
   },
   methods: {
-    getLatestProducts() {
-      axios
-        .get("/api/v1/latest-products/")
-        .then((response) => {
-          this.latestProducts = response.data;
+    async getLatestProducts() {
+      this.$store.commit('setIsLoading', true)
+      await axios
+        .get('/api/v1/latest-products/')
+        .then(response => {
+          this.latestProducts = response.data
         })
-        .catch((error) => {
-          console.log(error);
-        });
-    },
+        .catch(error => {
+          console.log(error)
+        })
+      this.$store.commit('setIsLoading', false)
+    }
   },
 };
 </script>
