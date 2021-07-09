e05a7675a20abe303193fa8e91a9fbf839fb2cd0
sicelo17 <70627871+sicelo17@users.noreply.github.com>
2021-07-09 11:13:20 +0200

    reformatted code

--- familytreelite-vue/src/views/Category.vue
+++ familytreelite-vue/src/views/Category.vue
@@ -1,27 +1,28 @@
 <template>
- <div class="page-category">
-        <div class="columns is-multiline">
-            <div class="column is-12">
-                <h2 class="is-size-2 has-text-centered">{{ category.name }}</h2>
-            </div>
+  <div class="page-category">
+    <div class="columns is-multiline">
+      <div class="column is-12">
+        <h2 class="is-size-2 has-text-centered">{{ category.name }}</h2>
+      </div>
 
-            <ProductBox 
-                v-for="product in category.products"
-                v-bind:key="product.id"
-                v-bind:product="product" />
-        </div>
+      <ProductBox
+        v-for="product in category.products"
+        v-bind:key="product.id"
+        v-bind:product="product"
+      />
     </div>
+  </div>
 </template>
 
 <script>
 import axios from "axios";
 import { toast } from "bulma-toast";
-import ProductBox from '@/components/ProductBox'
+import ProductBox from "@/components/ProductBox";
 
 export default {
   name: "Category",
   components: {
-    ProductBox
+    ProductBox,
   },
   data() {
     return {
