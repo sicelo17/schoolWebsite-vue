65c8da73d45ac3499859dcde6ca31898e005f7f1
sicelo17 <70627871+sicelo17@users.noreply.github.com>
2021-07-09 11:11:21 +0200

    added productbox to cateegory vue

--- familytreelite-vue/src/views/Category.vue
+++ familytreelite-vue/src/views/Category.vue
@@ -1,19 +1,28 @@
 <template>
-  <div class="page-category">
-    <div class="columns is-multiline">
-      <div class="column is-12">
-        <h2 class="is-size-2 has-text-centered">{{ category.name }}</h2>
-      </div>
+ <div class="page-category">
+        <div class="columns is-multiline">
+            <div class="column is-12">
+                <h2 class="is-size-2 has-text-centered">{{ category.name }}</h2>
+            </div>
+
+            <ProductBox 
+                v-for="product in category.products"
+                v-bind:key="product.id"
+                v-bind:product="product" />
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
 import axios from "axios";
 import { toast } from "bulma-toast";
+import ProductBox from '@/components/ProductBox'
 
 export default {
   name: "Category",
+  components: {
+    ProductBox
+  },
   data() {
     return {
       category: {
