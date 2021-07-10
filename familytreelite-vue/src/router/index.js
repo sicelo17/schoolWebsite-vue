e0c474afe19d52731dd7b86f832f645adc75d411
sicelo17 <70627871+sicelo17@users.noreply.github.com>
2021-07-10 17:21:00 +0200

    added myAccount route

--- familytreelite-vue/src/router/index.js
+++ familytreelite-vue/src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import store from '../store'
 import Home from '../views/Home.vue'
 import Product from '../views/Products.vue'
 import Category from '../views/Category.vue'
@@ -6,6 +7,7 @@ import Search from '../views/Search.vue'
 import Cart from '../views/Cart.vue'
 import SignUp from '../views/SignUp.vue'
 import LogIn from '../views/LogIn.vue'
+import MyAccount from '../views/MyAccount.vue'
 
 const routes = [
   {
@@ -32,6 +34,14 @@ const routes = [
     component: LogIn
   },
   {
+    path: '/my-account',
+    name: 'MyAccount',
+    component: MyAccount,
+    meta: {
+      requireLogin: true
+    }
+  },
+  {
     path: '/search',
     name: 'Search',
     component: Search
@@ -56,6 +66,14 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requireLogin) && !store.state.isAuthenticated) {
+    next({ name: 'LogIn', query: { to: to.path } });
+  } else {
+    next()
+  }
 })
 
 export default router
