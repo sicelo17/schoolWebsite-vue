43ccc1902645f1027e524dea3251e5121ec3daeb
sicelo17 <70627871+sicelo17@users.noreply.github.com>
2021-07-12 11:31:57 +0200

    added stripe secret key

--- familyTreeLite/familyTreeLite/settings.py
+++ familyTreeLite/familyTreeLite/settings.py
@@ -27,6 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+STRIPE_SECRET_KEY = 'sk_test_51HIHiuKBJV2qeWbD4IBpAODack7r7r9LJ0Y65zSFx7jUUwgy2nfKEgQGvorv1p2xp7tgMsJ5N9EW7K1lBdPnFnyK00kdrS27cj'
+
 
 # Application definition
 
@@ -44,7 +46,7 @@ INSTALLED_APPS = [
     'djoser',
 
     'product',
-
+    'order'
 ]
 
 CORS_ALLOWED_ORIGINS = [
