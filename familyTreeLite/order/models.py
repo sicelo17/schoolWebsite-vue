e776b6296f3697048d06458017bd3df92fee7133
sicelo17 <70627871+sicelo17@users.noreply.github.com>
2021-07-12 11:53:16 +0200

    created order model

--- familyTreeLite/order/models.py
+++ familyTreeLite/order/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+from django.db import models
+
+from product.models import Product
+
+class Order(models.Model):
+    user = models.ForeignKey(User, related_name='orders', on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    zipcode = models.CharField(max_length=100)
+    place = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    paid_amount = models.DecimalField(max_digits=8, decimal_places=2, blank=True, null=True)
+    stripe_token = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ['-created_at',]
+    
+    def __str__(self):
+        return self.first_name
